4d3f7f8bd57784b04449478f84f2a45239c9af84
EthanColbert8 <emcolbert2@gmail.com>
2024-11-22 20:43:57 -0500

    write a first cpp file

---
+++ src/fitter.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!\n";
+    std::cout << "\n";
+    std::cout << "Tesing whether skipping a line works.\n";
+
+    return 0;
+}
